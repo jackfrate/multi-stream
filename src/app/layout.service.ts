ca97baf9f7ab3f421772e3b2c01899fb6e6893b7
Jack Frate <jack.frate24@gmail.com>
2019-11-09 12:54:19 -0500

    really got the chat-service open state wired up this time

--- src/app/layout.service.ts
+++ src/app/layout.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnChanges, ViewChild, ElementRef } from '@angular/core';
-import { ChatSettings } from './free-objects/settings-interface';
+import { ChatService } from './chat.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +10,11 @@ export class LayoutService {
   layoutState: LayoutState;
   additionalSize: number;
 
-  // slider state
+  chatOpen: boolean;
 
-
-  constructor() {
-    this.layoutState = new StandardLayout();
+  constructor(private chatSvc: ChatService) {
+    this.chatOpen = this.chatSvc.chatOpen;
+    this.layoutState = new StandardLayout(this.chatOpen);
     this.additionalSize = 0;
   }
 
@@ -36,20 +36,41 @@ abstract class LayoutState {
   // this one is a little exaggerated so that the chat stays in the window right
   protected readonly tabHeight = 54; // 48 actual
   protected readonly headerHeight = 64; // 64 actual
+  protected readonly chatWidth = 350;
 
   // these are calculated
   protected innerX: number;
   protected innerY: number;
 
-  constructor() {
-    // const x1 =
-  }
+  constructor(public chatOpen: boolean) {
 
+  }
 
   // find a 16x9 ratio within the available space
   // TODO: make this run on window resize
-  setInner16x9() {
+  protected setInner16x9() {
+    const x1 = this.widthAdjuster(window.innerWidth);
+    const y1 = this.heightAdjuster(window.innerHeight);
 
+    // BIG BRAIN RATIO MATH BOIS
+    const x2 = ((16 * y1) / 9); // get the 16*9 x2 for y1
+    const y2 = ((9 * x1) / 16); // get the 16*9 y2 for x1
+
+
+
+    // now check which ones are gonna be inner x and y
+    // (its the lowest of x2 and y2)
+
+  }
+
+  protected widthAdjuster(totalWidth: number): number {
+    return this.chatOpen
+      ? totalWidth - this.chatWidth
+      : totalWidth;
+  }
+
+  protected heightAdjuster(totalHeight: number): number {
+    return totalHeight - this.headerHeight;
   }
 
   abstract getChatHeight(): number;
